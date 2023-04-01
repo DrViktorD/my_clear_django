aa8869bc082bcd05ca9229aeaa849dea2c9483c5
Виктор Дядищев <drviktord@gmail.com>
2023-04-01 20:29:02 +0300

    настройки

--- src/apps/manufacturers/apps.py
+++ src/apps/manufacturers/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ManufacturersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'manufacturers'
+    name = 'apps.manufacturers'
