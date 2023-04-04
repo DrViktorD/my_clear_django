b2c3b523bae84e272d5f90aebbd08bd7bc738150
Viktor Dyadishchev <124092767+DrViktorD@users.noreply.github.com>
2023-04-04 14:53:57 +0300

    Delete src/apps/staff directory

--- src/apps/staff/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from apps.staff.models import Position
-
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ('short_name',)
-    ordering = ['short_name']
-admin.site.register(Position, PositionAdmin)
